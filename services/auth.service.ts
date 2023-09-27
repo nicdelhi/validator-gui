774a8368c893993995135bcce8ae17f0c6724946
Mehdi Sabraoui <mehdi@shardeum.org>
2023-09-27 11:26:07 -0400

    modified so a hook isn't used in an async function

--- services/auth.service.ts
+++ services/auth.service.ts
@@ -1,26 +1,30 @@
 import Router from 'next/router'
 import { useGlobals } from '../utils/globals'
 import { hashSha256 } from '../utils/sha256-hash';
+import { useEffect } from 'react';
 
 const tokenKey = 'shmguitk'
 
-async function useLogin(password: string): Promise<void> {
+function useLogin(password: string): void {
   const { apiBase } = useGlobals()
-  const sha256digest = await hashSha256(password)
-  return fetch(`${apiBase}/auth/login`, {
-    headers: { 'Content-Type': 'application/json' },
-    method: 'POST',
-    body: JSON.stringify({ password: sha256digest }),
-  }).then(async (res) => {
-    const data = await res.json()
-    if (!res.ok) {
-      if (res.status === 403) {
-        throw new Error('Invalid password!')
+  useEffect(() => {
+      async function myFn() {
+      const sha256digest = await hashSha256(password)
+      const res = await fetch(`${apiBase}/auth/login`, {
+        headers: { 'Content-Type': 'application/json' },
+        method: 'POST',
+        body: JSON.stringify({ password: sha256digest }),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        if (res.status === 403) {
+          throw new Error('Invalid password!');
+        }
+        throw new Error('Error executing login');
       }
-      throw new Error('Error executing login')
+      localStorage.setItem(tokenKey, data.accessToken);
     }
-
-    localStorage.setItem(tokenKey, data.accessToken)
+    myFn()
   })
 }
 
