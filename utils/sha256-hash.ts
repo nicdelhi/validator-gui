77c0c41a9f0515d4ce80a1ec89ad6c0aefd5a02d
Tanuj Soni <tanuj@shardeum.org>
2024-01-11 17:10:26 +0530

    Minor fix in hashSha256 function

--- utils/sha256-hash.ts
+++ utils/sha256-hash.ts
@@ -4,7 +4,7 @@ export function getHashSalt(): string {
 
 export async function hashSha256(text: string): Promise<string> {
   const encoder = new TextEncoder();
-  const data = encoder.encode(text + staticSalt);
+  const data = encoder.encode(text);
   const hashBuffer = await crypto.subtle.digest('SHA-256', data);
   const hashArray = Array.from(new Uint8Array(hashBuffer));
   return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
